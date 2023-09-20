systemmonkey42/sshportal

commit 51d8669f19c7132672a6378722dede396783d857

feat: `user kick` and `user ban` commands
user kick: terminate all active user sessions (poll-based,
every 30s)

user ban: same thing but also removes all user keys, making him
unable to login again

These actions are logged and you can't perform action on yourself without the flag `force`

pkg/bastion/ssh.go
@@ -169,10 +169,21 @@ func ChannelHandler(srv *ssh.Server, conn *gossh.ServerConn, newChan gossh.NewCh
 				_ = ch.Close()
 				return
 			}
+			go func(cnx *gossh.ServerConn, dbConn *gorm.DB, sessionID uint) {
+				for {
+					sess := dbmodels.Session{Model: gorm.Model{ID: sessionID}, Status: string(dbmodels.SessionStatusActive)}
+					if err := dbConn.First(&sess).Error; err != nil || sess.Status != string(dbmodels.SessionStatusActive) {
+						log.Println("Session should be closed", sessionID, "closing connection")
+						conn.Close()
+						break
+					}
+					time.Sleep(30 * time.Second) // TODO: VDO: make configurable
+				}
+			}(conn, actx.db, sess.ID)
 			go func() {
 				err = multiChannelHandler(conn, newChan, ctx, sessionConfigs, sess.ID)
 				if err != nil {
-					log.Printf("Error: %v", err)
+					log.Printf("Error on session %v: %v", sess.ID, err)
 				}
 
 				now := time.Now()

pkg/bastion/shell.go
@@ -1612,6 +1612,59 @@ GLOBAL OPTIONS:
 						return enc.Encode(users)
 					},
 				}, {
+					Name:      "kick",
+					Usage:     "Kills all active sessions for user(s)",
+					ArgsUsage: "USER...",
+					Action: func(c *cli.Context) error {
+						if c.NArg() < 1 {
+							return cli.ShowSubcommandHelp(c)
+						}
+
+						if err := myself.CheckRoles([]string{"admin"}); err != nil {
+							return err
+						}
+						
+						var users []*dbmodels.User
+						if err := dbmodels.UsersByIdentifiers(db, c.Args()).Find(&users).Error; err != nil {
+							return err
+						}
+						
+						for _, user := range users {
+							if err := db.Model(&dbmodels.Session{}).Where(&dbmodels.Session{User: user, Status: string(dbmodels.SessionStatusActive)}).Update("status", "closed").Error; err != nil {
+								return err
+							}
+						}
+						return nil
+					},
+				}, {
+					Name:      "ban",
+					Usage:     "Kills all active sessions for user(s), and wipes all his ssh keys",
+					ArgsUsage: "USER...",
+					Action: func(c *cli.Context) error {
+						if c.NArg() < 1 {
+							return cli.ShowSubcommandHelp(c)
+						}
+
+						if err := myself.CheckRoles([]string{"admin"}); err != nil {
+							return err
+						}
+						
+						var users []*dbmodels.User
+						if err := dbmodels.UsersByIdentifiers(db, c.Args()).Find(&users).Error; err != nil {
+							return err
+						}
+						
+						for _, user := range users {
+							if err := db.Where("user_id = ?", user.ID).Delete(&dbmodels.UserKey{}).Error; err != nil {
+								return err
+							}
+							if err := db.Model(&dbmodels.Session{}).Where(&dbmodels.Session{User: user, Status: string(dbmodels.SessionStatusActive)}).Update("status", "closed").Error; err != nil {
+								return err
+							}
+						}
+						return nil
+					},
+				}, {
 					Name:        "invite",
 					ArgsUsage:   "<email>",
 					Usage:       "Invites a new user",

commit 9d44c0fbb505ca9df290a4fb7b1fb92423800c00

fix: `Error on session <session_id>: EOF` using port forwarding
Regression introduced with 51d8669f19c7132672a6378722dede396783d857

New go routine should only check `SessionStatusActive` to kill the session (initiated by a `user kick` or `user ban` for example)

pkg/bastion/ssh.go
@@ -169,17 +169,21 @@ func ChannelHandler(srv *ssh.Server, conn *gossh.ServerConn, newChan gossh.NewCh
 				_ = ch.Close()
 				return
 			}
-			go func(_ *gossh.ServerConn, dbConn *gorm.DB, sessionID uint) {
+			go func(_ *gossh.ServerConn, sessionID uint) {
 				for {
-					sess := dbmodels.Session{Model: gorm.Model{ID: sessionID}, Status: string(dbmodels.SessionStatusActive)}
-					if err := dbConn.First(&sess).Error; err != nil || sess.Status != string(dbmodels.SessionStatusActive) {
-						log.Println("Session should be closed", sessionID, "closing connection")
+					sess := dbmodels.Session{
+						Model:  gorm.Model{ID: sessionID},
+						Status: string(dbmodels.SessionStatusActive),
+					}
+
+					if sess.Status != string(dbmodels.SessionStatusActive) {
+						log.Println("Session", sessionID, "should be closed : closing connection")
 						conn.Close()
 						break
 					}
-					time.Sleep(30 * time.Second) // TODO: VDO: make configurable
+					time.Sleep(60 * time.Second)
 				}
-			}(conn, actx.db, sess.ID)
+			}(conn, sess.ID)
 			go func() {
 				err = multiChannelHandler(conn, newChan, ctx, sessionConfigs, sess.ID)
 				if err != nil {